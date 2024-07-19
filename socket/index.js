8aa19bbc4b31784d14072b51f30b47a797d7e316
Anurag <kanswalanurag123@gmail.com>
2024-07-19 17:55:27 +0530

    comit

--- socket/index.js
+++ socket/index.js
@@ -12,7 +12,7 @@ export const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-self-seven.vercel.app",
     method: ["GET", "DELETE", "POST", "PUT"],
     credentials: true,
   },
